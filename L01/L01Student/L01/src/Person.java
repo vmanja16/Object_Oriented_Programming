d16bdfae8bcda0d8daf57928de53a541bfc7eb58
Vikram Kajadi Manja <vmanja@ecegrid-thin1.ecn.purdue.edu>
2016-08-26 18:21:23 -0400

    Adding Homework 1

---
+++ L01/L01Student/L01/src/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+   private String lastName;
+   private String firstName;
+   private String dob; // DD/MM/YYYY
+
+   public Person(String ln, String fn, String fdob) {
+      lastName = ln;
+      firstName = fn;
+      dob = fdob;
+   }
+
+   public String toString( ) {
+     return firstName+" "+lastName+", born: "+dob;
+   }
+
+   public String getDOB( ) {
+      return dob;
+   }
+
+   public String getFirstName( ) {
+      return firstName;
+   }
+
+   public String getLastName( ) {
+      return lastName;
+   }
+}
