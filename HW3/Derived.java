ed6ce519498c51ebec0c6484b9fa9a6fa2a515e2
Vikram Kajadi Manja <vmanja@ecegrid-thin1.ecn.purdue.edu>
2016-09-03 01:22:57 -0400

    Finished HW3

---
+++ HW3/Derived.java
@@ -0,0 +1,15 @@
+public class Derived extends Base {
+
+   public Derived( ) { }
+
+  // Add any needed function(s) here.
+   public void f1(){
+	   System.out.println("Base f1");
+   }
+   
+   public void f1(float i){
+	   System.out.println("Derived f1(float)");
+   }
+
+}
+
