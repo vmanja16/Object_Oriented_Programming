29ba2f8320c16b9428b79f863a45cba9d09be82c
Vikram Kajadi Manja <vmanja@ecegrid-thin1.ecn.purdue.edu>
2016-09-03 01:00:53 -0400

    Completed Homework2

--- HW2/Base.java
+++ HW2/Base.java
@@ -2,7 +2,16 @@ public class Base {
 
    public Base( ) { }
 
-   // add necessary function(s) here 
+// add necessary function(s) here 
+   
+   public void f1(){
+	   System.out.println("Base f1");
+   }
+   
+   public void f2(){
+	   System.out.println("Base f2");
+   }
+   
 
 
 }
--- HW2/Derived.java
+++ HW2/Derived.java
@@ -4,5 +4,8 @@ public class Derived extends Base {
 
    // add necessary function(s) here.
 
+   public void f2(){
+	   System.out.println("Derived f2");
+   }
 }
 
